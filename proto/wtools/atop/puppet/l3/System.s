084f9b6cde2816b2209ea1e24d2f8f2567bc154c
Jack Shell <jackshel45@gmail.com>
2020-12-22 15:29:03 +0200

    Fixed to pass tests complete

--- proto/wtools/atop/puppet/l3/System.s
+++ proto/wtools/atop/puppet/l3/System.s
@@ -106,7 +106,7 @@ function formAssociates()
   }
 
   // _.assert( sys.strategy instanceof _.puppet.StrategyAbstract );
-  _.isPrototypeOf( _.puppet.StrategyAbstract, sys.strategy );
+  _.assert( _.prototypeIsPrototypeOf( _.puppet.StrategyAbstract, sys.strategy ) );
 
   return sys;
 }
